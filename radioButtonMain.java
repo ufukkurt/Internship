551bd962cfc1464ba8f7032ececb4236857155a5
ufukkurt <ufukkurt1@gmail.com>
2017-07-03 14:50:00 +0300

    Add new java files

---
+++ radioButtonMain.java
@@ -0,0 +1,15 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class radioButtonMain{
+
+	public static void main(String[] args) {
+		radioButton go = new radioButton();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go.setSize(600, 75);
+		go.setVisible(true);
+
+	}
+
+}
