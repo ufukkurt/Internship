8be2c1e0fe0277bc99840524c83a9c2b85501424
ufukkurt <ufukkurt1@gmail.com>
2017-07-19 14:22:31 +0300

    Update JSONRest.java

--- demo/src/main/java/JSONRest.java
+++ demo/src/main/java/JSONRest.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.tomcat.util.buf.StringUtils;
 import org.json.*;
@@ -39,7 +40,7 @@ public class JSONRest {
   public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
     InputStream is = new URL(url).openStream();
     try {
-      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
       String jsonText = readAll(rd);
       //System.out.println(jsonText);
       JSONObject json = new JSONObject(jsonText);
@@ -57,19 +58,27 @@ public class JSONRest {
 	int fromInfo = 0;
 	String mid = "&size=10&index=collector.logs-";
 	String tail = "&sort=reqNo";
+	String typeHead = "&type=";
 	
-	
-	JSONObject json = readJsonFromUrl(head + fromInfo + mid + dateInfo + tail);
+	Scanner typeInput = new Scanner(System.in);
+	String type;
 
+	System.out.print("Desired Type:(SDK or CLIENT)\n");
+	type = typeInput.next();
+
+	String upperType = type.toUpperCase(Locale.ENGLISH);
+	
+	JSONObject json = readJsonFromUrl(head + fromInfo + mid + dateInfo + tail + typeHead + upperType);
+	System.out.println(json);
     JSONObject deneme = json.getJSONObject("hits");
-
+    System.out.println(deneme);
     
     int total = deneme.getInt("total");
-   
+    System.out.println(total);
     
     	for(int a=0; a<((total/10)+1); a++) {
     		fromInfo = (a*10);
-    		JSONObject json1 = readJsonFromUrl(head + fromInfo + mid + dateInfo + tail);
+    		JSONObject json1 = readJsonFromUrl(head + fromInfo + mid + dateInfo + tail + typeHead + upperType);
         
     		JSONObject deneme1 = json1.getJSONObject("hits");
         
@@ -78,13 +87,17 @@ public class JSONRest {
     			JSONObject jsonArrayElement = jsonArray.getJSONObject(x);
     			JSONObject jsonArrayElementSource = jsonArrayElement.getJSONObject("_source");
     			System.out.println(jsonArrayElementSource.get("content"));
-    			String contentInfo = jsonArrayElementSource.getString("content") ;
+    			String contentInfo = jsonArrayElementSource.getString("content");
+    			System.out.println(jsonArrayElement.names());
+    			
+    			Object jsonArrayElementType = jsonArrayElement.get("_type");
+    			String typeInfo = jsonArrayElementType.toString();
             
     			try(FileWriter fileWriter = new FileWriter("C:\\Users\\pc\\Desktop\\sample.json", true);
             	    BufferedWriter bw = new BufferedWriter(fileWriter);
             	    PrintWriter out = new PrintWriter(bw))
             	{
-            	    out.println("Content [" + a + "-" + x + "] = " + contentInfo);
+            	    out.println(contentInfo);
             	    
             	} catch (IOException e) {
             	    
@@ -93,5 +106,6 @@ public class JSONRest {
     	}
     }
 }
+
 
  
